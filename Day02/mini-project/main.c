ddb59eec5b7c52f021bdcba4e1f85e9dcf5d2d39
ramiabdelghafour <itmeabdelghafour@gmail.com>
2025-09-04 06:45:03 +0100

    add full program

--- Day02/mini-project/main.c
+++ Day02/mini-project/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define M 50
-#define N 50
+#define M 500
+#define N 500
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
@@ -21,10 +21,10 @@ int main(int argc, char *argv[]) {
 	for(i = 0; i < n; i++){
 		printf("\n-- le livre N %d --\n", i+1);
 		printf("donner le titre de livre: ");
-		scanf("%s", &titre[incre]);
+		scanf("%s", titre[incre]);
 		
 		printf("donner le auteur de livre: ");
-		scanf("%s", &auteur[incre]);
+		scanf("%s", auteur[incre]);
 	
 		printf("donner le prix de livre: ");
 		scanf("%d", &prix[incre]);
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
 //- Afficher tous les livres disponibles.
 
 	printf("\n-- biblio --\n");
-	for(i = 0; i < n; i++){
+	for(i = 0; i < incre; i++){
 	
 		printf("\n-- le livre N %d --\n", i+1);
 		printf("le titre de livre: %s\n", titre[i]);
@@ -73,13 +73,8 @@ int main(int argc, char *argv[]) {
 	search = 0;
 	for(i = 0; i < incre ; i++){
 		if(strcmp(nom, titre[i]) == 0){
-			printf("\nLivre trouve:\n");
-			printf("le titre de livre: %s\n", titre[i]);
-			printf("le auteur de livre: %s\n", auteur[i]);
-			printf("le prix de livre: %d\n", prix[i]);
-			printf("le quantite de livre: %d\n", quantite[i]);
-			
 			int newquantite;
+			printf("le titre de livre: %s de quantite: %d\n", titre[i], quantite[i]);
 			printf("donner la nouvelle quantite: ");
 			scanf("%d", &newquantite);
 			quantite[i] = newquantite;
@@ -96,24 +91,19 @@ int main(int argc, char *argv[]) {
 		printf("ce livre n'exist pas dans cette biblio!");
 	}
 	
-	
 //- Supprimer un livre du stock.
-	printf("\npour supprimer un livre donner son titre: ");
+
+	printf("\nsupprimer un livre donner son titre: ");
 	scanf("%s", nom);
 	search = 0;
 	int j;
 	for(i = 0; i < incre ; i++){
 		if(strcmp(nom, titre[i]) == 0){
-			printf("\nle livre qu'on doit supprimer':\n");
-			printf("\nle titre de livre: %s\n", titre[i]);
-			printf("le auteur de livre: %s\n", auteur[i]);
-			printf("le prix de livre: %d\n", prix[i]);
-			printf("le quantite de livre: %d\n", quantite[i]);
-			
+			printf("\nlivre suprime: %s\n", titre[i]);
 			for(j = i; j < incre - 1; j++){
 				strcpy(titre[j], titre[j+1]);
 				strcpy(titre[j], titre[j+1]);
-				prix[i] = prix[j + 1];
+				prix[j] = prix[j + 1];
 			 	quantite[j] = quantite[j + 1];
 			}
  			incre--;
@@ -124,7 +114,7 @@ int main(int argc, char *argv[]) {
 		printf("ce livre n'exist pas dans cette biblio!");
 	}
 	printf("\n-- biblio --\n");
-	for(i = 0; i < n; i++){
+	for(i = 0; i < incre; i++){
 	
 		printf("\n-- le livre N %d --\n", i+1);
 		printf("le titre de livre: %s\n", titre[i]);
@@ -132,6 +122,9 @@ int main(int argc, char *argv[]) {
 		printf("le prix de livre: %d\n", prix[i]);
 		printf("le quantite de livre: %d\n", quantite[i]);
 	}
+	
+//- Afficher le nombre total de livres en stock.
 
+	printf("\nle nombre total de livres %d", incre);
 	return 0;
 }
